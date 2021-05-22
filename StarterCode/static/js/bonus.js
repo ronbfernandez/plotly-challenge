ad662870fb49b2e59f3c9fb055f519d1509881da
Ron Fernandez <ronbfernandez@gmail.com>
2021-05-22 16:35:53 -0700

    updating further code on bonus js

--- StarterCode/static/js/bonus.js
+++ StarterCode/static/js/bonus.js
@@ -11,5 +11,24 @@ function buildGauge(wfreq) {
     var x = radius * Math.cos(radians);
     var y = radius * Math.sin(radians);
 
-    
+    var mainPath = "M -.0 -0.05 L .0 0.05 L ";
+    var pathX = String(x);
+    var space = " ";
+    var pathY = String(y);
+    var pathEnd = "Z";
+    var path = mainPath.concat(pathX, space, pathY, pathEnd);
+
+    var data = [
+        {
+            type: "scatter",
+            x: [0]
+            y: [0]
+            marker: { size: 12, color: "850000" },
+            showlegend: false,
+            name: "Freq",
+            text: level,
+            hoverinfo: "text+name"
+        },
+        {}
+    ]
 }
