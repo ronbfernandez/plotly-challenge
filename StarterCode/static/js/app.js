7bb97d59abc1eb2cb2dde04b826a904fdd0991d2
Ron Fernandez <ronbfernandez@gmail.com>
2021-05-22 23:01:45 -0700

    code rebuild done to resolve mistake in logic and order

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,127 +1,28 @@
+// code rebuild done to resolve mistake in logic
 
+function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata= data.metadata;
+      var resultsarray= metadata.filter(sampleobject => sampleobject.id == sample);
+      var result= resultsarray[0]
+      var PANEL = d3.select("#sample-metadata");
+      PANEL.html("");
+      Object.entries(result).forEach(([key, value]) => {
+        PANEL.append("h6").text(`${key}: ${value}`);
+      });
 
-
-
-
-
-
-
-
-// had issues in getting the samples to read in to the each section (Demographic Info)
-// horizontal bar chart.  was not able to figure out how to fix on var metadata_panel. 
-
-
-var drawChart = function(x_data, y_data, hoverText, metadata) {
-
-    var metadata_panel = d3.select("#sample-metadata");
-    metadata_panel.html("");
-    Object.entries(metadata).forEach(([key, value]) => {
-        metadata_panel.append("p").text('${key}: ${value}');
     });
+  }
 
-    var trace = {
-        x: x_data,
-        y: y_data,
-        text: hoverText,
-        type: 'bar',
-        orientation: 'h'
-    };
 
-    var data = [trace];
 
-    var layout = {
-        title: "Top 10 OTU",
-        yaxis:{
-            tickmode: "linear",
-        },
-        margin: {
-            l: 100,
-            r: 100,
-            t: 100,
-            b: 30
-        }
-    };
 
-    Plotly.newPlot('bar', data);
 
-    var trace2 = {
-        x: x_data,
-        y: y_data,
-        text: hoverText,
-        mode: 'markers',
-        marker: {
-            size: y_data,
-            color: x_data
-        }
-    }; 
 
-    var data2 = [trace2];
 
-    Plotly.newPlot('bubble', data2);
-
-};
-
-var populateDropdown = function(names) {
-
-    var selectTag = d3.select("#selDataset");
-    var options = selectTag.selectAll('option').data(names);
-
-    options.enter()
-        .append('option')
-        .attr('value', function(d) {
-            return d;
-        })
-        .text(function(d) {
-            return d;
-        });
-    
-};
-
-var optionChanged = function(newValue) {
-
-    d3.json("samples.json").then(function(data) {
-        
-        sample_new = data["samples"].filter(function(sample) {
-
-            return sample.id == newValue;
-
-        });
-
-        metadata_new = data["metadata"].filter(function(metadata) {
-
-            return metadata.id == newValue;
-
-        });
 
  
-    });
 
-    x_data = sample_new[0]["otu_ids"];
-    y_data = sample_new[0]["sample_values"];
-    hoverText = sample_new[0]["otu_labels"];
-
-    console.log(x_data);
-    console.log(y_data);
-    console.log(hoverText);
-
-    drawChart(x_data, y_data, hoverText, metadata_new[0]);
-    };
-
-d3.json("samples.json").then(function(data) {
-
-    //Populate dropdown with names
-    populateDropdown(data["names"]);
-    
-    //Populate the page with the first value
-    x_data = data["samples"][0]["otu_ids"];
-    y_data = data["samples"][0]["sample_values"];
-    hoverText = data["samples"][0]["otu_labels"];
-    metadata = data["metadata"][0];
-
-    //Draw the chart on load
-    drawChart(x_data, y_data, hoverText, metadata);
-
-});
 
 
 
